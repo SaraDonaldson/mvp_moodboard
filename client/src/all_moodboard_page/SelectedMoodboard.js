ff5372b5457554122d732abb6a504cd9e9370277
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-03-05 15:54:56 +0000

    implemented simple drag and resize

--- client/src/all_moodboard_page/SelectedMoodboard.js
+++ client/src/all_moodboard_page/SelectedMoodboard.js
@@ -1,51 +1,97 @@
 import React, { useState } from "react";
 import "./SelectedMoodboard.css";
+import { Rnd } from "react-rnd";
+
+
+
 
 const SelectedMoodboard = ({board1,backgroundStyle, handleSelectedImage}) => {
   console.log(board1);
   const mystyle = backgroundStyle;
   const [isSelected, setIsSelected]= useState(false);
+  const [sizeState, setSizeState] = useState({ width: 200,
+    height: 200})
+ 
   
   function handleSelect(imageUrl){
     handleSelectedImage(imageUrl);
     setIsSelected(true);
   }
 
+
   return (
+
+    
+
     <div className='displayArea'  style={mystyle}>
-      {board1.map((element, index) => {
+
+     {board1.map((element, index) => {
         if (element.type === "label") {
           return (
+
+            <Rnd
+            default={{
+              x: 0,
+              y: 0,
+              width: 320,
+              height: 200,
+            }}
+            key={index}
+            >
+   
             <p
-              style={{
+              draggable= {false}
+              style= {{
                 position: "absolute",
                 top: `${element.top}%`,
                 left: `${element.left}%`,
                 zIndex: 5,
+                
               }}
-              key={index}
             >
               {element.value}
             </p>
+            </Rnd>
           );
         } else {
           return (
-            <img
-            alt= ""
-            className= {isSelected ? "selected-image" : ""}
+            <Rnd
+            default={{
+              x: 0,
+              y: 0,
+              width: 320,
+              height: 200,
+            }}
+            key={index}
+          >
+              <div 
+              className="mb-image" 
+              onClick={() => {handleSelect(`${element.value}`)}}
               style={{
-                position: "absolute",
+                minWidth:'100%',
+                minHeight:'100%',
+            }}
+             >
+
+            <img 
+            draggable= {false}
+            className={isSelected ? "selected-image" : ""} 
+            alt= ""
+            style={{
                 top: `${element.top}%`,
                 left: element.left+'%',
-                // zIndex: 1,
-                
+                zIndex: 1,
+                minWidth:'100%',
+                minHeight:'100%'        
               }}
-              key={index}
-              src={element.value}
-              onClick={() => {handleSelect(`${element.value}`)}}/>
+              src={element.value}/>
+              
+              </div>
+              </Rnd>
           );
         }
       })}
+   
     </div>
   );
 };
