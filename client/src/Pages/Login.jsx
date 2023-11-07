99bf0577a961b1c292ece91b4fe2e032eecc402f
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-11-07 13:10:45 +0000

    create login page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import LoginForm from '../Components/LoginSignUp/LoginForm.jsx'
+import "../Styles/Login.css";
 
 function Login() {
+
+
+
+
   return (
-    <div>login</div>
+    <div className='login-sign-up'>
+
+        <div className='form-container'>
+    <LoginForm/>
+        </div>
+
+    </div>
   )
 }
 
