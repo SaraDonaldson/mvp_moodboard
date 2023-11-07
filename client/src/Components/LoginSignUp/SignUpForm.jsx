f1b559ce41a28cf2397a6202c1e64e70aa702c04
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-11-07 13:11:39 +0000

    create sign up form

---
+++ client/src/Components/LoginSignUp/SignUpForm.jsx
@@ -0,0 +1,60 @@
+
+import React, { useState } from "react";
+
+
+
+function SignUpForm() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); 
+    function validateForm() {
+      return email.length > 0 && password.length > 0;
+  
+    }
+  
+    function handleSubmit(event) {
+  
+      event.preventDefault();
+  
+    }
+  return (
+    <div className='login-form'>
+       <h1>Create an Account</h1>
+      <form onSubmit={handleSubmit}>
+<div className="form-field">
+  <label>Email</label>
+  <input
+    autoFocus
+    type="email"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+  />
+</div>
+<div className="form-field">
+  <label>Password</label>
+  <input
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+  />
+</div>
+
+
+<button className="form-button" type="submit" disabled={!validateForm()}>
+    Create Account
+</button>
+
+</form>
+    </div>
+  )
+}
+
+export default SignUpForm
+
+
+
+
+
+   
+
+
